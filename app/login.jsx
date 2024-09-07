73b327ddf91bb7634ba1309c1c09daf3567fcfaa
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-09-07 10:38:03 +0300

    login

---
+++ app/login.jsx
@@ -0,0 +1,48 @@
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+  } from "react-native";
+  import React from "react";
+  import { Link } from "expo-router";
+import { Button } from "react-native-web";
+  const login = () => {
+    return (
+      <View style={{backgroundColor:'#ff0011', flex: 1 }}>
+            <Link href='/login'>
+              <TouchableOpacity onpress={()=>{console.log('login')}}>
+              <Text style={styles.link}>kidinka</Text>
+                </TouchableOpacity>
+              <View style={{
+                width: 10,
+                height: 10,
+                backgroundColor:'white',
+                borderRadius: 5
+
+              }}/>
+            </Link>     
+        <View>          
+          </View>      
+      </View>
+    );
+  };
+  
+  export default login;
+  
+  const styles = StyleSheet.create({
+    container: {
+      paddingLeft: 5,
+      paddingRight: 5,
+      flex: 1,
+      justifyContent:'center',
+      alignItems:'center',
+      backgroundColor: "#FF0000",
+    },
+    link: {
+      fontSize:50,
+      color:'white',
+      fontWeight:'800'
+    },
+  });
+  
